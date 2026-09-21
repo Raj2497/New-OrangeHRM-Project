33e7db6d0868cbafd759afef33cc04c2a8ff03f7
Raj2497 <v.rajkumar2497@gmail.com>
2026-09-21 11:23:38 +0530

    Tester-2 in BaseClass

--- src/test/java/com/orangehrm/utility/BaseClass.java
+++ src/test/java/com/orangehrm/utility/BaseClass.java
@@ -91,7 +91,18 @@ public class BaseClass {
 	public void closeBrowser() {
 		driver.quit();
 	}
-	
+	public void test21() {
+
+		System.out.println("test21");
+	}
+	public void test22() {
+
+		System.out.println("test22");
+	}
+	public void test23() {
+
+		System.out.println("test23");
+	}
 	
 
 }
